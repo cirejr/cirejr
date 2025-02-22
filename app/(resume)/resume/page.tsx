11a905162a3ee7871bf494c8590f7b7cfc003f67
Cire Jr <juniorcireba@gmail.com>
2025-02-22 01:47:43 +0000

    updated breakpoint

--- app/(resume)/resume/page.tsx
+++ app/(resume)/resume/page.tsx
@@ -4,7 +4,7 @@ export default function ResumeFr() {
   return (
     <div className="container mx-auto max-w-5xl p-8 text-sm">
       <header className="mb-8 grid grid-cols-1 gap-8 sm:grid-cols-3">
-        <div className="md:col-span-2">
+        <div className="sm:col-span-2">
           <h1 className="text-6xl font-bold text-blue-600">Ciré Jr Ba</h1>
           <p className="mt-2">
             Experienced IT Consultant and Front-End Developer specializing in
@@ -20,7 +20,7 @@ export default function ResumeFr() {
         </div>
       </header>
       <main className="grid grid-cols-1 gap-8 sm:grid-cols-3">
-        <section className="md:col-span-2">
+        <section className="sm:col-span-2">
           <h2 className="mb-4 text-2xl font-bold text-blue-600">
             Professional Experience
           </h2>
