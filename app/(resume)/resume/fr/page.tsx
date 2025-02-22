b3d696366074b350720d4aa67dab79f66afe4b88
Cire Jr <juniorcireba@gmail.com>
2025-02-22 00:09:43 +0000

    updated resume

--- app/(resume)/resume/fr/page.tsx
+++ app/(resume)/resume/fr/page.tsx
@@ -1,39 +1,50 @@
 export default function ResumeFr() {
   return (
-    <div className='container max-w-5xl mx-auto p-8 text-sm'>
-      <header className='grid grid-cols-1 sm:grid-cols-3 gap-8 mb-8 '>
-        <div className='sm:col-span-2'>
-          <h1 className='text-6xl font-bold text-blue-600'>Ciré Jr Ba</h1>
-          <p className='mt-2'>
+    <div className="container mx-auto max-w-5xl p-8 text-sm">
+      <header className="mb-8 grid grid-cols-1 gap-8 sm:grid-cols-3">
+        <div className="sm:col-span-2">
+          <h1 className="text-6xl font-bold text-blue-600">Ciré Jr Ba</h1>
+          <p className="mt-2">
             Consultant IT expérimenté et développeur front-end spécialisé dans
             le développement d&apos;applications web et mobiles
           </p>
         </div>
-        <div className='mt-8'>
+        <div className="mt-8">
           <p>juniorcireba@gmail.com</p>
           <p>(+221) 77 842 73 00</p>
-          <p>github.com/cirejr</p>
-          <p>linkedin.com/in/ba-ciré-junior</p>
+
+          <a target="_blank" href="https://www.github.com/cirejr">
+            github.com/cirejr
+          </a>
+          <br />
+          <a target="_blank" href="https://www.linkedin.com/in/ba-ciré-junior">
+            linkedin.com/in/ba-ciré-junior
+          </a>
+
+          <br />
+          <a href="https://x.com/cirejr_" target="_blank">
+            x.com/cirejr_
+          </a>
         </div>
       </header>
-      <main className='grid grid-cols-1 sm:grid-cols-3 gap-8'>
-        <section className='sm:col-span-2'>
-          <h2 className='text-2xl font-bold text-blue-600 mb-4'>
+      <main className="grid grid-cols-1 gap-8 sm:grid-cols-3">
+        <section className="sm:col-span-2">
+          <h2 className="mb-4 text-2xl font-bold text-blue-600">
             Expérience Professionnelle
           </h2>
-          <div className='mb-8'>
-            <div className='flex items-center gap-4'>
-              <h3 className='text-lg'>
-                Développeur ·<span className='font-semibold'> Freelance</span>
+          <div className="mb-8">
+            <div className="flex items-center gap-4">
+              <h3 className="text-lg">
+                Développeur ·<span className="font-semibold"> Freelance</span>
               </h3>
-              <p className='text-sm text-gray-600'>Jan 2021 — Présent</p>
+              <p className="text-sm text-gray-600">Jan 2021 — Présent</p>
             </div>
-            <ul className='list-disc ml-5 mt-2 space-y-1'>
+            <ul className="ml-5 mt-2 list-disc space-y-1">
               <li>
                 Membre de l&apos;équipe front-end pour une plateforme
                 d&apos;apprentissage en ligne adaptée aux étudiants en médecine,
                 utilisant des technologies telles que
-                <strong className='ml-1'>
+                <strong className="ml-1">
                   Next.js, TypeScript, Shadcn, Next-ui, et Tailwind CSS
                 </strong>
                 . La plateforme propose une section publique semblable à un
@@ -51,7 +62,7 @@ export default function ResumeFr() {
               <li>
                 Collaboration au développement front-end d&apos;une application
                 de gestion de niveau entreprise utilisant des technologies comme{" "}
-                <strong className='ml-1'>
+                <strong className="ml-1">
                   Next js, Typescript, Shadcn, Tailwind CSS.
                 </strong>{" "}
                 L&apos;application propose diverses fonctionnalités telles que
@@ -97,18 +108,18 @@ export default function ResumeFr() {
               </li>
             </ul>
           </div>
-          <div className='mb-8'>
-            <div className='flex items-center gap-4'>
-              <h3 className='text-lg'>
+          <div className="mb-8">
+            <div className="flex items-center gap-4">
+              <h3 className="text-lg">
                 Développeur Junior ·
-                <span className='font-semibold'>
+                <span className="font-semibold">
                   {" "}
                   Digital & Telecom Services
                 </span>
               </h3>
-              <p className='text-sm text-gray-600'>Jul 2021 — Oct 2021</p>
+              <p className="text-sm text-gray-600">Jul 2021 — Oct 2021</p>
             </div>
-            <ul className='list-disc ml-5 mt-2 space-y-1'>
+            <ul className="ml-5 mt-2 list-disc space-y-1">
               <li>
                 Développement d&apos;un e-marché WordPress pour une entreprise
                 de légumes
@@ -128,15 +139,15 @@ export default function ResumeFr() {
               </li>
             </ul>
           </div>
-          <div className='mb-8'>
-            <div className='flex items-center gap-4'>
-              <h3 className='text-lg'>
+          <div className="mb-8">
+            <div className="flex items-center gap-4">
+              <h3 className="text-lg">
                 Développeur WordPress ·
-                <span className='font-semibold'> GOMTUHG</span>
+                <span className="font-semibold"> GOMTUHG</span>
               </h3>
-              <p className='text-sm text-gray-600'>Déc 2020 — Jul 2021</p>
+              <p className="text-sm text-gray-600">Déc 2020 — Jul 2021</p>
             </div>
-            <ul className='list-disc ml-5 mt-2 space-y-1'>
+            <ul className="ml-5 mt-2 list-disc space-y-1">
               <li>Développement de sites WordPress pour divers clients</li>
               <li>
                 Création d&apos;une plateforme de vente en ligne de noms de
@@ -150,93 +161,116 @@ export default function ResumeFr() {
           </div>
         </section>
         <aside>
-          <div className='mb-8'>
-            <h2 className='text-2xl font-bold text-blue-600 mb-4'>
+          <div className="mb-8">
+            <h2 className="mb-4 text-2xl font-bold text-blue-600">
               Compétences
             </h2>
-            <h3 className='text-lg font-semibold'>Langages de Programmation</h3>
-            <p className='text-sm'>
+            <h3 className="text-lg font-semibold">Langages de Programmation</h3>
+            <p className="text-sm">
               HTML, CSS, JavaScript, Typescript, C (basique), PHP (basique),
               Java (basique)
             </p>
-            <h3 className='text-lg font-semibold mt-4'>
+            <h3 className="mt-4 text-lg font-semibold">
               Frameworks et Bibliothèques
             </h3>
-            <p className='text-sm'>
+            <p className="text-sm">
               React Js, React Native, Expo, Angular, Next.js, Bootstrap,
               Tailwind CSS
             </p>
-            <h3 className='text-lg font-semibold mt-4'>
+            <h3 className="mt-4 text-lg font-semibold">
               Outils et Plateformes
             </h3>
-            <p className='text-sm'>
+            <p className="text-sm">
               Git, GitHub, GitLab, WordPress, PrestaShop, Sanity,
-              Hygraph/GraphCMS, Figma
+              Hygraph/GraphCMS, Figma, Supabase
             </p>
           </div>
-          <div className='mb-8'>
-            <h2 className='text-2xl font-bold text-blue-600 mb-4'>
+          <div className="mb-8">
+            <h2 className="mb-4 text-2xl font-bold text-blue-600">
               Projets Sélectionnés
             </h2>
-            <h3 className='text-lg font-semibold mt-4'>
-              Développement d&apos;une application de prise de rendez-vous
-              médical
+
+            <h3 className="mt-4 text-lg font-semibold">
+              Excel-Genie : Générateur de Tableaux Excel Alimenté par l&apos;IA
             </h3>
-            <p className='text-sm'>
-              Inscription des utilisateurs, formulaire complexe de création de
-              profil utilisateur, formulaire de prise de rendez-vous, tableau de
-              bord administratif, liste des rendez-vous, annulation de
-              rendez-vous et notifications par SMS. Appwrite en tant que
-              backend, Next js - Typescript en tant que frontend, Shadcn et
-              Tailwind CSS pour le style.
+            <p className="text-sm">
+              Un outil intelligent permettant de générer des fichiers Excel
+              structurés via une interface similaire à ChatGPT. Développé avec{" "}
+              <strong>Next.js, TypeScript, Tailwind CSS, shadcn</strong>, et{" "}
+              <strong>Supabase</strong> pour l&apos;authentification et le
+              backend. L&apos;IA est propulsée par le{" "}
+              <strong>Vercel AI SDK</strong>.
             </p>
-            <h3 className='text-lg font-semibold mt-4'>
-              Développement d&apos;une plateforme e-learning
-            </h3>
-            <p className='text-sm'>
-              Page d&apos;accueil, tableau de bord administratif, formulaires de
-              création de cours et d&apos;utilisateur, authentification. Backend
-              propulsé par <strong>Supabase</strong> frontend et style avec{" "}
+
+            <h3 className="mt-4 text-lg font-semibold">SaaS Starter Kit</h3>
+            <p className="text-sm">
+              Un template SaaS prêt à l&apos;emploi pour accélérer le
+              développement, intégrant l&apos;authentification, la gestion des
+              utilisateurs, la facturation et un tableau de bord. Conçu avec{" "}
               <strong>
-                Nextjs, TypeScript, Tailwind CSS, Shadcn et Next-ui
+                Next.js, TypeScript, Tailwind CSS, shadcn, Supabase, Authjs,
+                Stripe, et Paddle
               </strong>
               .
             </p>
-            <h3 className='text-lg font-semibold mt-4'>Site web de blog</h3>
-            <p className='text-sm'>
-              Développement d&apos;un site web Next js, Tailwind CSS avec
-              GraphCMS pour la gestion de contenu, avec section de commentaires
+
+            <h3 className="mt-4 text-lg font-semibold">
+              Application de Prise de Rendez-Vous Médicaux
+            </h3>
+            <p className="text-sm">
+              Une plateforme permettant aux utilisateurs de s&apos;inscrire, de
+              créer des profils détaillés, de réserver des rendez-vous et de
+              recevoir des notifications par SMS. Elle comprend un tableau de
+              bord administrateur pour gérer les réservations. Développée avec{" "}
+              <strong>Next.js, TypeScript, Tailwind CSS, shadcn</strong>, et{" "}
+              <strong>Appwrite</strong> pour le backend.
+            </p>
+
+            <h3 className="mt-4 text-lg font-semibold">
+              Plateforme d&apos;E-Learning
+            </h3>
+            <p className="text-sm">
+              Une plateforme complète d&apos;apprentissage en ligne avec une
+              page d&apos;accueil, un tableau de bord administrateur, la gestion
+              des cours et des utilisateurs, ainsi que l&apos;authentification.
+              Backend alimenté par <strong>Supabase</strong>, avec un frontend
+              utilisant{" "}
+              <strong>
+                Next.js, TypeScript, Tailwind CSS, shadcn et Next-UI
+              </strong>
+              .
             </p>
           </div>
-          <div className='mb-8'>
-            <h2 className='text-2xl font-bold text-blue-600 mb-4'>Éducation</h2>
-            <h3 className='text-lg font-semibold'>
+
+          <div className="mb-8">
+            <h2 className="mb-4 text-2xl font-bold text-blue-600">Éducation</h2>
+            <h3 className="text-lg font-semibold">
               Université Cheikh Anta Diop
             </h3>
-            <p className='text-sm'>
+            <p className="text-sm">
               Licence en Transmission de Données et Sécurité de
               l&apos;Information
             </p>
-            <p className='text-sm text-gray-600'>2017 — 2019</p>
-            <h3 className='text-lg font-semibold mt-4'>
+            <p className="text-sm text-gray-600">2017 — 2021</p>
+            <h3 className="mt-4 text-lg font-semibold">
               Université Virtuelle du Sénégal
             </h3>
-            <p className='text-sm'>
+            <p className="text-sm">
               Première Année en Développement d&apos;Applications Web et Jeux
               Vidéo
             </p>
-            <p className='text-sm text-gray-600'>2017 — 2018</p>
+            <p className="text-sm text-gray-600">2017 — 2018</p>
           </div>
-          <div className='mb-8'>
-            <h2 className='text-2xl font-bold text-blue-600 mb-4'>Langues</h2>
-            <p className='text-sm'>
+          <div className="mb-8">
+            <h2 className="mb-4 text-2xl font-bold text-blue-600">Langues</h2>
+            <p className="text-sm">
               Français (Parlé & Écrit), Anglais (Parlé & Écrit), Bambara (Langue
               Maternelle)
             </p>
           </div>
-          <div className='mb-8'>
-            <h2 className='text-2xl font-bold text-blue-600 mb-4'>Intérêts</h2>
-            <p className='text-sm'>Lecture, Jeux vidéo, Sports</p>
+          <div className="mb-8">
+            <h2 className="mb-4 text-2xl font-bold text-blue-600">Intérêts</h2>
+            <p className="text-sm">Lecture, Jeux vidéo, Sports</p>
           </div>
         </aside>
       </main>
--- app/(resume)/resume/page.tsx
+++ app/(resume)/resume/page.tsx
@@ -2,41 +2,50 @@ import Link from "next/link";
 
 export default function ResumeFr() {
   return (
-    <div className='max-w-5xl mx-auto p-8 text-sm'>
-      <header className='grid grid-cols-1 md:grid-cols-3 gap-8 mb-8'>
-        <div className='md:col-span-2'>
-          <Link href='/' className='text-4xl font-bold text-blue-600'>
+    <div className="mx-auto max-w-5xl p-8 text-sm">
+      <header className="mb-8 grid grid-cols-1 gap-8 md:grid-cols-3">
+        <div className="md:col-span-2">
+          <Link href="/" className="text-6xl font-bold text-blue-600">
             Ciré Jr Ba
           </Link>
-          <p className='text-lg mt-2'>
+          <p className="mt-2">
             Experienced IT Consultant and Front-End Developer specializing in
             web and mobile application development
           </p>
         </div>
-        <div className='mt-8'>
+        <div className="mt-8">
           <p>juniorcireba@gmail.com</p>
           <p>(+221) 77 842 73 00</p>
-          <p>github.com/cirejr</p>
-          <p>linkedin.com/in/ba-ciré-junior</p>
+          <a target="_blank" href="https://www.github.com/cirejr">
+            github.com/cirejr
+          </a>
+          <br />
+          <a target="_blank" href="https://www.linkedin.com/in/ba-ciré-junior">
+            linkedin.com/in/ba-ciré-junior
+          </a>
+          <br />
+          <a href="https://x.com/cirejr_" target="_blank">
+            x.com/cirejr_
+          </a>
         </div>
       </header>
-      <main className='grid grid-cols-1 md:grid-cols-3 gap-8'>
-        <section className='md:col-span-2'>
-          <h2 className='text-2xl font-bold text-blue-600 mb-4'>
+      <main className="grid grid-cols-1 gap-8 md:grid-cols-3">
+        <section className="md:col-span-2">
+          <h2 className="mb-4 text-2xl font-bold text-blue-600">
             Professional Experience
           </h2>
-          <div className='mb-8'>
-            <div className='flex items-center gap-4'>
-              <h3 className='text-lg'>
-                Developer ·<span className='font-semibold'> Freelance</span>
+          <div className="mb-8">
+            <div className="flex items-center gap-4">
+              <h3 className="text-lg">
+                Developer ·<span className="font-semibold"> Freelance</span>
               </h3>
-              <p className='text-sm text-gray-600'>Jan 2021 — Present</p>
+              <p className="text-sm text-gray-600">Jan 2021 — Present</p>
             </div>
-            <ul className='list-disc ml-5 mt-2 space-y-1'>
+            <ul className="ml-5 mt-2 list-disc space-y-1">
               <li>
                 Part of the front-end team for an online learning platform
                 tailored for medical students, leveraging technologies like
-                <strong className='ml-1'>
+                <strong className="ml-1">
                   Next.js, TypeScript, Shadcn, Next-ui, and Tailwind CSS
                 </strong>
                 . The platform features a social media-like public section for
@@ -52,7 +61,7 @@ export default function ResumeFr() {
               <li>
                 Collaborated on the front-end development of an enterprise-level
                 management application using technologies like{" "}
-                <strong className='ml-1'>
+                <strong className="ml-1">
                   Next js, Typescript, Shadcn, Tailwind CSS.
                 </strong>{" "}
                 The app features various functionnalities such as role-based
@@ -93,18 +102,18 @@ export default function ResumeFr() {
               </li>
             </ul>
           </div>
-          <div className='mb-8'>
-            <div className='flex items-center gap-4'>
-              <h3 className='text-lg'>
+          <div className="mb-8">
+            <div className="flex items-center gap-4">
+              <h3 className="text-lg">
                 Junior Developer ·
-                <span className='font-semibold'>
+                <span className="font-semibold">
                   {" "}
                   Digital & Telecom Services
                 </span>
               </h3>
-              <p className='text-sm text-gray-600'>Jul 2021 — Oct 2021</p>
+              <p className="text-sm text-gray-600">Jul 2021 — Oct 2021</p>
             </div>
-            <ul className='list-disc ml-5 mt-2 space-y-1'>
+            <ul className="ml-5 mt-2 list-disc space-y-1">
               <li>Built a worpdress e-market for a legume company</li>
               <li>
                 Built a prestashop e-commerce website for a glasses business
@@ -119,15 +128,15 @@ export default function ResumeFr() {
               </li>
             </ul>
           </div>
-          <div className='mb-8'>
-            <div className='flex items-center gap-4'>
-              <h3 className='text-lg'>
+          <div className="mb-8">
+            <div className="flex items-center gap-4">
+              <h3 className="text-lg">
                 Wordpress developper ·
-                <span className='font-semibold'> GOMTUHG</span>
+                <span className="font-semibold"> GOMTUHG</span>
               </h3>
-              <p className='text-sm text-gray-600'>Dec 2020 — Jul 2021</p>
+              <p className="text-sm text-gray-600">Dec 2020 — Jul 2021</p>
             </div>
-            <ul className='list-disc ml-5 mt-2 space-y-1'>
+            <ul className="ml-5 mt-2 list-disc space-y-1">
               <li>Developed WordPress websites for various clients</li>
               <li>Created an online domain name and hosting sales platform</li>
               <li>
@@ -137,84 +146,104 @@ export default function ResumeFr() {
           </div>
         </section>
         <aside>
-          <div className='mb-8'>
-            <h2 className='text-2xl font-bold text-blue-600 mb-4'>Skills</h2>
-            <h3 className='text-lg font-semibold'>Programming Languages</h3>
-            <p className='text-sm'>
+          <div className="mb-8">
+            <h2 className="mb-4 text-2xl font-bold text-blue-600">Skills</h2>
+            <h3 className="text-lg font-semibold">Programming Languages</h3>
+            <p className="text-sm">
               HTML, CSS, JavaScript, C (basic), PHP (basic), Java
             </p>
-            <h3 className='text-lg font-semibold mt-4'>
+            <h3 className="mt-4 text-lg font-semibold">
               Frameworks & Libraries
             </h3>
-            <p className='text-sm'>
+            <p className="text-sm">
               React Js, React Native, Expo, Angular, Next.js, Bootstrap,
               Tailwind CSS
             </p>
-            <h3 className='text-lg font-semibold mt-4'>Tools & Platforms</h3>
-            <p className='text-sm'>
+            <h3 className="mt-4 text-lg font-semibold">Tools & Platforms</h3>
+            <p className="text-sm">
               Git, GitHub, GitLab, WordPress, PrestaShop, Sanity,
-              Hygraph/GraphCMS, Figma
+              Hygraph/GraphCMS, Figma, Supabase
             </p>
           </div>
-          <div className='mb-8'>
-            <h2 className='text-2xl font-bold text-blue-600 mb-4'>
+          <div className="mb-8">
+            <h2 className="mb-4 text-2xl font-bold text-blue-600">
               Selected Projects
             </h2>
-            <h3 className='text-lg font-semibold mt-4'>
-              Build a medical care appointment booking app
+
+            <h3 className="mt-4 text-lg font-semibold">
+              Excel-Genie: AI-Powered Spreadsheet Generator
             </h3>
-            <p className='text-sm'>
-              User registration, complex user profile creation form, appointment
-              booking form, admin dashboard, appointment listing, appointment
-              cancellation, and sms notifications. Appwrite as backend, next js
-              - Typescript as frontend, shadcn and tailwind css for styling.
+            <p className="text-sm">
+              An AI-driven tool for generating structured Excel spreadsheets
+              through a ChatGPT-like interface. Built with{" "}
+              <strong>Next.js, TypeScript, Tailwind CSS, shadcn</strong>, and{" "}
+              <strong>Supabase</strong> for authentication and backend. AI
+              capabilities powered by <strong>Vercel AI SDK</strong>.
             </p>
-            <h3 className='text-lg font-semibold mt-4'>
-              Built an e-learning platform
-            </h3>
-            <p className='text-sm'>
-              Landing page, admin dashboard, course and user creation forms,
-              auth. Backend powered by <strong>Supabase</strong> frontend and
-              styling with{" "}
+
+            <h3 className="mt-4 text-lg font-semibold">SaaS Starter Kit</h3>
+            <p className="text-sm">
+              A pre-built SaaS template designed for rapid development,
+              including authentication, user management, billing integration,
+              and dashboard UI. Developed using{" "}
               <strong>
-                Nextjs, TypeScript, Tailwind CSS, shadcn and next-ui
+                Next.js, TypeScript, Tailwind CSS, shadcn, Supabase, Authjs,
+                Stripe, and Paddle
               </strong>
               .
             </p>
-            <h3 className='text-lg font-semibold mt-4'>Blog website</h3>
-            <p className='text-sm'>
-              Built a next js, tailwind css website with Graphcms for content
-              management, with comment section
+
+            <h3 className="mt-4 text-lg font-semibold">
+              Medical Care Appointment Booking App
+            </h3>
+            <p className="text-sm">
+              A platform enabling users to register, create detailed profiles,
+              book appointments, and receive SMS notifications. Features an
+              admin dashboard for managing appointments. Built with{" "}
+              <strong>Next.js, TypeScript, Tailwind CSS, shadcn</strong>, and{" "}
+              <strong>Appwrite</strong> as the backend.
+            </p>
+
+            <h3 className="mt-4 text-lg font-semibold">E-Learning Platform</h3>
+            <p className="text-sm">
+              A complete online learning platform with a landing page, admin
+              dashboard, course and user management, and authentication. Backend
+              powered by <strong>Supabase</strong>, with the frontend using{" "}
+              <strong>
+                Next.js, TypeScript, Tailwind CSS, shadcn, and Next-UI
+              </strong>
+              .
             </p>
           </div>
-          <div className='mb-8'>
-            <h2 className='text-2xl font-bold text-blue-600 mb-4'>Education</h2>
-            <h3 className='text-lg font-semibold'>
+
+          <div className="mb-8">
+            <h2 className="mb-4 text-2xl font-bold text-blue-600">Education</h2>
+            <h3 className="text-lg font-semibold">
               Cheikh Anta Diop University
             </h3>
-            <p className='text-sm'>
+            <p className="text-sm">
               Bachelor&apos;s Degree in Data Transmission and Information
               Security
             </p>
-            <p className='text-sm text-gray-600'>2017 — 2019</p>
-            <h3 className='text-lg font-semibold mt-4'>
+            <p className="text-sm text-gray-600">2017 — 2021</p>
+            <h3 className="mt-4 text-lg font-semibold">
               Senegal Virtual University
             </h3>
-            <p className='text-sm'>
+            <p className="text-sm">
               First Year in Web Gaming Application Development
             </p>
-            <p className='text-sm text-gray-600'>2017 — 2018</p>
+            <p className="text-sm text-gray-600">2017 — 2018</p>
           </div>
-          <div className='mb-8'>
-            <h2 className='text-2xl font-bold text-blue-600 mb-4'>Languages</h2>
-            <p className='text-sm'>
+          <div className="mb-8">
+            <h2 className="mb-4 text-2xl font-bold text-blue-600">Languages</h2>
+            <p className="text-sm">
               French (Spoken & Written), English (Spoken & Written), Bambara
               (Native)
             </p>
           </div>
-          <div className='mb-8'>
-            <h2 className='text-2xl font-bold text-blue-600 mb-4'>Interests</h2>
-            <p className='text-sm'>Reading, Video games, Sports</p>
+          <div className="mb-8">
+            <h2 className="mb-4 text-2xl font-bold text-blue-600">Interests</h2>
+            <p className="text-sm">Reading, Video games, Sports</p>
           </div>
         </aside>
       </main>
